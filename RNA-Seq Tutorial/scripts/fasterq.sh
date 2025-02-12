1ff65f381d9736d8ed33f25388142e656499ddf4
jtm77 <jtmorellano@gmail.com>
2025-02-12 17:28:00 -0600

    updating scripts

--- RNA-Seq Tutorial/scripts/fasterq.sh
+++ RNA-Seq Tutorial/scripts/fasterq.sh
@@ -1,21 +1,13 @@
 #!/bin/bash
+threads=4
+ourout_dir="./fastq_output"
+mkdir -p "$output_dir"
 
-# Set number of threads
-THREADS=4
-
-# Set output directory (modify as needed)
-OUTPUT_DIR="./fastq_output"
-mkdir -p "$OUTPUT_DIR"
-
-# Loop through all SRR accession numbers found in the directory
 for SRR in $(ls | grep -oE "SRR[0-9]+"); do
-    echo "Processing $SRR..."
-    
-    # Run fasterq-dump
-    fasterq-dump --threads $THREADS --split-files --outdir "$OUTPUT_DIR" "$SRR"
-    
-    # Compress output (optional but recommended)
-    pigz "$OUTPUT_DIR/$SRR"*.fastq
+    echo "working on $SRR..."
+        fasterq-dump --threads $threads --split-files --outdir "$output_dir" "$SRR"
+
+    pigz "$output_dir/$SRR"*.fastq
 done
 
-echo "All files processed!"
+echo "lol, it's done!"
--- RNA-Seq Tutorial/scripts/kallisto_quant.sh
+++ RNA-Seq Tutorial/scripts/kallisto_quant.sh
@@ -6,7 +6,7 @@ output="/mnt/c/Users/morel/OneDrive/Desktop/Pressure/Tutorial/quant"
 threads=4
 bootstraps=50
 mkdir -p "$output"
-# Define the read pairs and output names
+
 declare -A samples=(
     ["Brain2_0.1"]="SRR15927837_1_val_1.fq.gz SRR15927837_2_val_2.fq.gz"
     ["Brain4_9.0"]="SRR15927811_1_val_1.fq.gz SRR15927811_2_val_2.fq.gz"
@@ -14,7 +14,6 @@ declare -A samples=(
     ["Brain2_9.0"]="SRR15927813_1_val_1.fq.gz SRR15927813_2_val_2.fq.gz"
     )
 
-# Loop through the samples and run Kallisto
 for sample in "${!samples[@]}"; do
     echo "Processing $sample..."
     kallisto quant \
